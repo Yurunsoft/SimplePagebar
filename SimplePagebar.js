0e0eedd58535de08877aa4632e7c3195192454af
宇润 <admin@yurunsoft.com>
2016-12-09 11:50:12 +0800

    第一次提交

---
+++ SimplePagebar.js
@@ -0,0 +1,159 @@
+/**
+ * 一个使用TypeScript开发的简单分页条，支持首页、末页、上一页、下一页、自定义页码切换。
+ * 宇润软件 http://www.yurunsoft.com
+ */
+var SimplePagebar = (function () {
+    function SimplePagebar(option) {
+        this.container = null;
+        this.element = null;
+        this.pages = 0;
+        this.currPage = 1;
+        this.showFirst = true;
+        this.showLast = true;
+        this.first = '&lt;&lt;';
+        this.last = '&gt;&gt;';
+        this.prev = '&lt;';
+        this.next = '&gt;';
+        this.onPageChange = null;
+        for (var key in option) {
+            this[key] = option[key];
+        }
+    }
+    SimplePagebar.prototype.display = function () {
+        var _this = this;
+        if (SimplePagebar.isFirstLoad) {
+            var item = $('<link>').attr({ rel: 'stylesheet', type: 'text/css', href: SimplePagebar.currPath + 'SimplePagebar.css' });
+            $('head').append(item);
+            SimplePagebar.isFirstLoad = false;
+        }
+        this.element = $('<div class="simple-pagebar">\
+            <a class="sp-first"></a>\
+            <a class="sp-prev"></a>\
+            <input type="text" class="sp-page"/>\
+            <a class="sp-next"></a>\
+            <a class="sp-last"></a>\
+        </div>');
+        if (this.showFirst) {
+            this.element.find('.sp-first')
+                .on('click', function () {
+                _this.goto(1);
+                _this.setPageShowText();
+            })
+                .html(this.first);
+        }
+        else {
+            this.element.find('.sp-first').remove();
+        }
+        if (this.showLast) {
+            this.element.find('.sp-last')
+                .on('click', function () {
+                _this.goto(_this.pages);
+                _this.setPageShowText();
+            })
+                .html(this.last);
+        }
+        else {
+            this.element.find('.sp-last').remove();
+        }
+        this.element.find('.sp-prev')
+            .on('click', function () {
+            _this.goto(Math.max(1, _this.currPage - 1));
+            _this.setPageShowText();
+        })
+            .html(this.prev);
+        this.element.find('.sp-next')
+            .on('click', function () {
+            _this.goto(Math.min(_this.pages, _this.currPage + 1));
+            _this.setPageShowText();
+        })
+            .html(this.next);
+        this.container.html('').append(this.element);
+        this.element.find('.sp-page')
+            .on('focus', function (e) {
+            _this.setPageShowText();
+            $(e.currentTarget).select();
+        })
+            .on('blur', function (e) {
+            var page = parseInt($(e.currentTarget).val());
+            if (!isNaN(page)) {
+                _this.goto(page);
+            }
+            _this.setPageShowText();
+        })
+            .on('keydown', function (e) {
+            if (e.which == 13) {
+                $(e.currentTarget).blur();
+            }
+            else if (e.which == 27) {
+                $(e.currentTarget).val(_this.currPage);
+                $(e.currentTarget).blur();
+            }
+        });
+        this.setPageShowText();
+    };
+    SimplePagebar.prototype.goto = function (page) {
+        if (page < 0) {
+            page = 1;
+        }
+        else if (page > this.pages) {
+            page = this.pages;
+        }
+        var oldPage = this.currPage;
+        this.currPage = page;
+        if (this.onPageChange !== null) {
+            this.onPageChange({ page: this.currPage, isChanged: oldPage != this.currPage });
+        }
+    };
+    SimplePagebar.prototype.setPageShowText = function () {
+        var page = this.element.find('.sp-page');
+        if (page.is(":focus")) {
+            page.val(this.currPage);
+        }
+        else {
+            page.val(this.currPage + " / " + this.pages);
+        }
+    };
+    SimplePagebar.isFirstLoad = true;
+    SimplePagebar.currPath = '';
+    return SimplePagebar;
+}());
+/*!
+ * referrence:
+ * https://github.com/fsjohnhuang/getCurrAbsPath
+ */
+;
+(function (exports) {
+    var doc = exports.document, a = {}, expose = +new Date(), rExtractUri = /((?:http|https|file):\/\/.*?\/[^:]+)(?::\d+)?:\d+/, isLtIE8 = ('' + doc.querySelector).indexOf('[native code]') === -1;
+    function getCurrAbsPath() {
+        // FF,Chrome
+        if (doc.currentScript) {
+            return doc.currentScript.src;
+        }
+        var stack;
+        try {
+            a.b();
+        }
+        catch (e) {
+            stack = e.fileName || e.sourceURL || e.stack || e.stacktrace;
+        }
+        // IE10
+        if (stack) {
+            var absPath = rExtractUri.exec(stack)[1];
+            if (absPath) {
+                return absPath;
+            }
+        }
+        // IE5-9
+        for (var scripts = doc.scripts, i = scripts.length - 1, script; script = scripts[i--];) {
+            if (script.className !== expose && script.readyState === 'interactive') {
+                script.className = expose;
+                // if less than ie 8, must get abs path by getAttribute(src, 4)
+                return isLtIE8 ? script.getAttribute('src', 4) : script.src;
+            }
+        }
+    }
+    var arr = getCurrAbsPath().split("/");
+    delete arr[arr.length - 1];
+    SimplePagebar.currPath = arr.join("/");
+}(window));
+//# sourceMappingURL=SimplePagebar.js.map
